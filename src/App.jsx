7daea898194addaa51483aff6563a812e13d232c
HamdiNur <hamdoonur26@gmail.com>
2024-08-24 19:48:49 +0300

    imported signup and login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,7 +14,13 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
-          
+          <Route path="/signup" element={<SignUp /> }/>
+          <Route path="/login" element={<Login/>}/>
+
+
+
+
+           
         </Routes>
       </div>
     </Router>
