628590c0fa4a7c52203d3dafce9fa92e69c3f11f
HamdiNur <hamdoonur26@gmail.com>
2024-08-24 19:51:13 +0300

    added shadow

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -24,8 +24,8 @@ const Contact = () => {
 
   return (
     <div className="py-24 bg-[#E0F7FA]">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="grid lg:grid-cols-2 grid-cols-1">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
+        <div className="grid lg:grid-cols-2 grid-cols-1 shadow-xl">
           <div className="lg:mb-0 mb-10">
             <div className="group w-full h-full">
               <div className="relative h-full">
